41b84201cbecf19a11ef91d16188eb8ea22eef03
Polovoy Vlad <vlad.polovoy@gmail.com>
2023-04-27 16:14:54 +0300

    Set up the DB

---
+++ Dir_Struct/Data/DbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Dir_Struct.Models;
+
+namespace Dir_Struct.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(FolderContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Folder_Entities.Any())
+                return;
+
+            var folders = new Folder_Entity[]
+            {
+                new Folder_Entity{ID=1, Name="Creating Digital Images", OwnerID=0},
+                new Folder_Entity{ID=2, Name="Resources", OwnerID=1},
+                new Folder_Entity{ID=3, Name="Evidence", OwnerID=1},
+                new Folder_Entity{ID=4, Name="Graphic Products", OwnerID=1},
+                new Folder_Entity{ID=5, Name="Primary Sources", OwnerID=2},
+                new Folder_Entity{ID=6, Name="Secondary Sources", OwnerID=2},
+                new Folder_Entity{ID=7, Name="Process", OwnerID=4},
+                new Folder_Entity{ID=8, Name="Final Product", OwnerID=4}
+            };
+            foreach (Folder_Entity f in folders)
+                context.Folder_Entities.Add(f);
+            context.SaveChanges();
+        }
+    }
+}
+
---
+++ Dir_Struct/Data/FolderContext.cs~
@@ -0,0 +1,20 @@
+﻿using Dir_Struct.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dir_Struct.Data
+{
+    public class FolderContext : DbContext
+    {
+        public FolderContext(DbContextOptions<FolderContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Folder_Entity> Folder_Entities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Folder_Entity>().ToTable("Folder_Entity");
+        }
+    }
+}
+
