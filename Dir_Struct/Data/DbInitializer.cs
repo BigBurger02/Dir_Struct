95be514022819ccdb162e69a253de0da230be2bb
Polovoy Vlad <vlad.polovoy@gmail.com>
2023-04-28 13:46:20 +0300

    Changed main directory from <Creating digital images> to </>

--- Dir_Struct/Data/DbInitializer.cs
+++ Dir_Struct/Data/DbInitializer.cs
@@ -15,14 +15,15 @@ namespace Dir_Struct.Data
 
             var folders = new Folder_Entity[]
             {
-                new Folder_Entity{ID=1, Name="Creating Digital Images", OwnerID=0},
-                new Folder_Entity{ID=2, Name="Resources", OwnerID=1},
-                new Folder_Entity{ID=3, Name="Evidence", OwnerID=1},
-                new Folder_Entity{ID=4, Name="Graphic Products", OwnerID=1},
-                new Folder_Entity{ID=5, Name="Primary Sources", OwnerID=2},
-                new Folder_Entity{ID=6, Name="Secondary Sources", OwnerID=2},
-                new Folder_Entity{ID=7, Name="Process", OwnerID=4},
-                new Folder_Entity{ID=8, Name="Final Product", OwnerID=4}
+                new Folder_Entity{ID=1, Name="/", OwnerID=0},
+                new Folder_Entity{ID=2, Name="Creating Digital Images", OwnerID=1},
+                new Folder_Entity{ID=3, Name="Resources", OwnerID=2},
+                new Folder_Entity{ID=4, Name="Evidence", OwnerID=2},
+                new Folder_Entity{ID=5, Name="Graphic Products", OwnerID=2},
+                new Folder_Entity{ID=6, Name="Primary Sources", OwnerID=3},
+                new Folder_Entity{ID=7, Name="Secondary Sources", OwnerID=3},
+                new Folder_Entity{ID=8, Name="Process", OwnerID=5},
+                new Folder_Entity{ID=9, Name="Final Product", OwnerID=5}
             };
             foreach (Folder_Entity f in folders)
                 context.Folder_Entities.Add(f);
